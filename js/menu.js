cd011a0b6aa72224dc80fdae0aab35a1b929853f
Katryne-Kov <katykovvv@gmail.com>
2022-11-18 23:35:16 +0200

    add img

--- js/menu.js
+++ js/menu.js
@@ -4,7 +4,7 @@
     closeMenuBtn: document.querySelector("[data-menu-close]"),
     menu: document.querySelector("[data-menu]"),
     body: document.querySelector("body"),
-    menuList: document.querySelector(".mob-list"),
+    // menuList: document.querySelector(".nav-list"),
   };
   refs.openMenuBtn.addEventListener("click", toggleMenu);
   refs.closeMenuBtn.addEventListener("click", toggleMenu);
@@ -13,8 +13,8 @@
     refs.menu.classList.toggle("is-hidden");
     refs.body.classList.toggle("no-scroll");
   }
-  function removeMenu() {
-    refs.menu.classList.add("is-hidden");
-    refs.body.classList.remove("no-scroll");
-  }
+  // function removeMenu() {
+  //   refs.menu.classList.add("is-hidden");
+  //   refs.body.classList.remove("no-scroll");
+  // }
 })();
